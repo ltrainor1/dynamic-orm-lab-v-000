6b892a02feb698972ed00c01048c57749e7a9a2e
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-20 22:19:28 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,47 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+attr_accessor :id
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+    sql = "PRAGMA table_info('#{table_name}')"
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+    column_names.compact
+
+self.column_names.each do |col_name|
+  attr_accessor col_name.to_sym
+end
+
+end 
+
+def initialize(options={})
+
+  options.each do |property, value|
+    self.send("#{property}=", value)
+  end
+end
+
+def some_instance_method
+  self.class.some_class_method
+end
+
+def table_name_for_insert
+  self.class.table_name
+end
+
+def col_names_for_insert
+  self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+end
+
 end
